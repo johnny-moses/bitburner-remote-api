04eae0b1ed0b57ff065e51e38aac3e0ef76f9add
Johnny <johnny.moses3@gmail.com>
2023-12-05 22:06:08 -0600

    script paths fixed

--- src/wormy/simple/jack.js
+++ src/wormy/simple/jack.js
@@ -7,8 +7,8 @@ export async function main(ns) {
     const target = ns.args[0];
     const hostServer = ns.getHostname();
     const securityThreshold = ns.getServerMinSecurityLevel(target) + 10; // Security buffer
-    const moneyThreshold = 0.75; // 75% of max money
-    const ramBuffer = 2.5; // Reserve 12GB of RAM
+    const moneyThreshold = 0.90; // 75% of max money
+    const ramBuffer = 2.5; // Reserve 2.5GB of RAM
 
     if (!target) {
         ns.tprint("Error: No target specified. Exiting.");
@@ -33,7 +33,7 @@ export async function main(ns) {
                 action = 'hack';
             }
 
-            const scriptRam = ns.getScriptRam(`/${action}.js`, hostServer);
+            const scriptRam = ns.getScriptRam(`wormy/simple/scripts/${action}.js`, hostServer);
             if (scriptRam <= 0) {
                 ns.tprint(`Error: Script RAM usage is zero or invalid for ${action}.js on server ${hostServer}`);
                 return;
@@ -41,18 +41,18 @@ export async function main(ns) {
 
             let availableRam = ns.getServerMaxRam(hostServer) - ns.getServerUsedRam(hostServer) - ramBuffer;
             while (availableRam >= scriptRam) {
-                const pid = ns.exec(`/${action}.js`, hostServer, 1, target);
+                const pid = ns.exec(`wormy/simple/scripts/${action}.js`, hostServer, 1, target);
                 if (pid === 0) {
                     ns.tprint(`Failed to start ${action}.js on target: ${target} from server ${hostServer}.`);
                     break; // Break the loop if unable to start a new process
                 } else {
-                    ns.tprint(`Launched ${action}.js on target: ${target} from server ${hostServer}. PID: ${pid}`);
+                    // ns.tprint(`Launched ${action}.js on target: ${target} from server ${hostServer}. PID: ${pid}`);
                     availableRam -= scriptRam;
                 }
                 await ns.sleep(100); // Short sleep to prevent script from hogging CPU
             }
 
-            ns.tprint(`Not enough RAM on server ${hostServer} to perform ${action} on target: ${target}. Waiting to retry...`);
+            // ns.tprint(`Not enough RAM on server ${hostServer} to perform ${action} on target: ${target}. Waiting to retry...`);
             await ns.sleep(5000);
         } catch (e) {
             ns.tprint(`An error occurred on server ${hostServer}: ${e}`);
