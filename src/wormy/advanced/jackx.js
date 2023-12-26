6ecb116c86e8e281176a50891c42df3ad832f6c5
johnny-moses <johnny.moses3@gmail.com>
2023-12-26 15:50:09 -0600

    Refactor advanced attack script for better performance
    The advanced attack script (jackx.js) was significantly refactored for better performance and manageability. The script now operates from 'homeServer', uses less RAM buffer, and targets top 3 servers rather than a provided list. In addition, the batch job distribution logic was streamlined and error handling was improved.

--- src/wormy/advanced/jackx.js
+++ src/wormy/advanced/jackx.js
@@ -1,75 +1,61 @@
+/** @param {NS} ns **/
 export async function main(ns) {
     const homeServer = 'home';
-    const ramBuffer = 1.25; // Reserve 2.5GB of RAM
+    const ramBuffer = 0.50; // Reserve 1.25GB of RAM
 
-    let targetServers = scanAllServers(ns);
-    let rootServers = getRootServers(ns, targetServers);
-    let eligibleServersList = eligibleServers(ns, targetServers).slice(0, 3); // Top 3 servers
+    // Pass the target server from the arguments
+    let targetServer = ns.args[0];
+    if (!targetServer) {
+        ns.tprint('No target server provided');
+        return;
+    }
 
-    ns.tprint(`Starting advanced attack script from home server: ${homeServer}`);
+    ns.tprint(`Starting advanced attack script on target server: ${targetServer}`);
+
+    const rootServers = getRootServers(ns);
 
     // eslint-disable-next-line no-constant-condition
     while (true) {
         await ns.sleep(1000);
-        for (let target of eligibleServersList) {
-            for (let source of rootServers) {
-                await ns.sleep(5000);
-                let action;
-                const currentSecurity = ns.getServerSecurityLevel(target.name);
-                const currentMoney = ns.getServerMoneyAvailable(target.name);
-                const maxMoney = ns.getServerMaxMoney(target.name);
-                const securityThreshold = ns.getServerMinSecurityLevel(target.name) + 10;
-                const moneyThreshold = 0.90;
+        for (let source of rootServers) {
+            await ns.sleep(5000);
+            let action;
+            const currentSecurity = ns.getServerSecurityLevel(targetServer);
+            const currentMoney = ns.getServerMoneyAvailable(targetServer);
+            const maxMoney = ns.getServerMaxMoney(targetServer);
+            const securityThreshold = ns.getServerMinSecurityLevel(targetServer) + 10;
+            const moneyThreshold = 0.90;
 
-                if (currentSecurity > securityThreshold) {
-                    action = 'weaken';
-                } else if (currentMoney < maxMoney * moneyThreshold) {
-                    action = 'grow';
+            if (currentSecurity > securityThreshold) {
+                action = 'weaken';
+            } else if (currentMoney < maxMoney * moneyThreshold) {
+                action = 'grow';
+            } else {
+                action = 'hack';
+            }
+            const scriptRam = ns.getScriptRam(`wormy/advanced/scripts/${action}.js`, homeServer);
+            if (scriptRam <= 0) {
+                ns.tprint(`Error: Script RAM usage is zero or invalid for ${action}.js on home server ${homeServer}`);
+                return;
+            }
+            let availableRam = ns.getServerMaxRam(source) - ns.getServerUsedRam(source) - ramBuffer;
+            while (availableRam >= scriptRam) {
+                ns.scp(`wormy/advanced/scripts/${action}.js`, homeServer, source);
+                const pid = ns.exec(`wormy/advanced/scripts/${action}.js`, source, 1, targetServer);
+                if (pid === 0) {
+                    break;
                 } else {
-                    action = 'hack';
+                    availableRam -= scriptRam;
                 }
-                const scriptRam = ns.getScriptRam(`wormy/simple/scripts/${action}.js`, homeServer);
-                if (scriptRam <= 0) {
-                    ns.tprint(`Error: Script RAM usage is zero or invalid for ${action}.js on home server ${homeServer}`);
-                    return;
-                }
-                let availableRam = ns.getServerMaxRam(source) - ns.getServerUsedRam(source) - ramBuffer;
-                while (availableRam >= scriptRam) {
-                    ns.scp(`wormy/simple/scripts/${action}.js`, homeServer, source);
-                    const pid = ns.exec(`wormy/simple/scripts/${action}.js`, source, 1, target.name);
-                    if (pid === 0) {
-                        break;
-                    } else {
-                        availableRam -= scriptRam;
-                    }
-                    await ns.sleep(100);
-                }
-            }
-        }
-        targetServers = scanAllServers(ns);
-        rootServers = getRootServers(ns, targetServers);
-        eligibleServersList = eligibleServers(ns, targetServers).slice(0, 3);
-    }
-}
-
-
-// This function scans for all servers & returns the list
-function scanAllServers(ns) {
-    let servers = ['home'];
-
-    for (let i = 0; i < servers.length; ++i) {
-        let newServers = ns.scan(servers[i]);
-        for (let newServer of newServers) {
-            if (!servers.includes(newServer)) {
-                servers.push(newServer);
+                await ns.sleep(100);
             }
         }
     }
-    return servers;
 }
 
 // Function checks for all servers that you have root access to
-function getRootServers(ns, servers) {
+function getRootServers(ns) {
+    let servers = ns.scan('home');
     let rootServers = [];
 
     for (let server of servers) {
@@ -79,21 +65,4 @@ function getRootServers(ns, servers) {
     }
 
     return rootServers;
-}
-
-// Function to check which servers are hackable
-function eligibleServers(ns, servers) {
-    let hackableServers = [];
-
-    for (let i = 0; i < servers.length; ++i) {
-        let server = servers[i];
-        if (ns.getServerRequiredHackingLevel(server) <= ns.getHackingLevel()) {
-            hackableServers.push({
-                name: server,
-                money: ns.getServerMoneyAvailable(server),
-            });
-        }
-    }
-    hackableServers.sort((a, b) => b.money - a.money); // Sort in descending order of money
-    return hackableServers;
 }
