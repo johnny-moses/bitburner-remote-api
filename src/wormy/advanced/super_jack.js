0af9736081de82faa593673b4bae6a60817e0283
Johnny <johnny.moses3@gmail.com>
2023-12-27 15:53:09 -0600

    Update messaging and add growPercent formula in jackx.js
    The code in `jackx.js` has been updated to include a call to the `growPercent` formula after successfully starting the advanced attack script. Also, enhancements were made to notification messages to turn them from basic status info into more meaningful success and warning messages.

--- src/wormy/advanced/super_jack.js
+++ src/wormy/advanced/super_jack.js
@@ -22,9 +22,26 @@ export async function main(ns) {
     const formulas = ns.formulas.hacking;
 
     while (true) {
-        await ns.sleep(100);
+        // Default to 5 seconds if no scripts are running
+        let maxRunTime = 5000;
         for (let source of rootServers) {
+            const scriptsRunning = ns.ps(source);
+            for (let script of scriptsRunning) {
+                // Validate timeRunning before using it
+                if (script.timeRunning && !isNaN(script.timeRunning)) {
+                    maxRunTime = Math.max(maxRunTime, script.timeRunning);
+                }
+            }
+        }
+        // Wait for max running time plus a bit buffer if scripts are running
+        if (maxRunTime > 5000) {
+            await ns.sleep((maxRunTime * 1000) + 100);
+        } else {
+            // If no scripts are running, just sleep for the default interval
             await ns.sleep(100);
+        }
+
+        for (let source of rootServers) {
             let action, threads, scriptPath;
             const player = ns.getPlayer();
             const server = ns.getServer(targetServer);
@@ -41,28 +58,25 @@ export async function main(ns) {
                 action = 'weaken';
                 threads = calculateWeakenThreads(ns, targetServer);
                 scriptPath = 'wormy/advanced/scripts/weaken.js';
-            } else if (currentMoney < maxMoney * moneyThreshold) {
+            } else if ((currentMoney / maxMoney) < moneyThreshold) {
                 action = 'grow';
-                threads = calculateGrowThreads(ns, targetServer);
+                threads = calculateGrowThreads(ns, targetServer, 2); // 2x server's money
                 scriptPath = 'wormy/advanced/scripts/grow.js';
             } else {
                 action = 'hack';
-                threads = 1;  // for hack we'll run single thread as its impact is linear
+                threads = calculateHackThreads(ns, targetServer, 0.5);  // hack 50% of the money
                 scriptPath = 'wormy/advanced/scripts/hack.js';
             }
 
-            const cores = availableRam / ns.getScriptRam(scriptPath, homeServer);
-
             const scriptRam = ns.getScriptRam(scriptPath, homeServer);
             if (scriptRam <= 0) {
-                ns.print(`ERROR: Script RAM usage is zero or invalid for ${scriptPath} on home server ${homeServer}`);
+                ns.print(`WARN: Script RAM usage is zero or invalid for ${scriptPath} on home server ${homeServer}`);
                 return;
             }
 
             if ((threads * scriptRam) > availableRam) {
                 ns.print(`WARN: Not enough RAM available on ${source} for ${scriptPath} with ${threads} threads.`);
                 continue;
-
             }
 
             ns.scp(scriptPath, source);
@@ -107,19 +121,21 @@ function tryNuke(ns, server) {
 }
 
 // Weaken threads calculation function
-function calculateWeakenThreads(ns, server) {
-    const oldSecurity = ns.getServerSecurityLevel(server);
-    const newSecurity = ns.getServerMinSecurityLevel(server) + 10;
-    const amountWeakened = ns.weakenAnalyze(1);
-    const threads = Math.ceil((oldSecurity - newSecurity) / amountWeakened);
-    return Math.max(1, threads); // at least 1 thread
+function calculateWeakenThreads(ns, targetServer) {
+    const oldSecurity = ns.getServerSecurityLevel(targetServer);
+    const securityThreshold = ns.getServerMinSecurityLevel(targetServer) + 10;
+    const weakenEffect = ns.weakenAnalyze(1);
+    return Math.ceil((oldSecurity - securityThreshold) / weakenEffect);
 }
 
 // Grow threads calculation function
-function calculateGrowThreads(ns, server) {
-    const oldMoney = ns.getServerMoneyAvailable(server);
-    const newMoney = ns.getServerMaxMoney(server);
-    const growthPercentage = ns.getServerGrowth(server) / 100;
-    const threads = Math.ceil(Math.log(newMoney / oldMoney) / Math.log(1 + growthPercentage));
-    return Math.max(1, threads); // at least 1 thread s
+function calculateGrowThreads(ns, targetServer, desiredMultiplier) {
+    // Use growthAnalyze to calculate required threads for achieving the desired multiplier
+    return Math.ceil(ns.growthAnalyze(targetServer, desiredMultiplier));
+}
+
+function calculateHackThreads(ns, targetServer, availablePercentage) {
+    const maxMoney = ns.getServerMaxMoney(targetServer);
+    const hackAmount = maxMoney * availablePercentage; // percentage of maxMoney you want to hack
+    return Math.ceil(ns.hackAnalyzeThreads(targetServer, hackAmount));
 }
