7d6bd8d1eeaa22063176a6c7565814cce797ee1a
Johnny <johnny.moses3@gmail.com>
2023-12-30 01:36:52 -0600

    Refactored 'deploy-jack.js' to recursively run 'jackx.js' on connected servers
    The code now checks if 'jackx.js' is already running on each server, avoiding unnecessary re-executions. To further enhance the deployment, it scans and recursively runs the script on each server's connected servers as well. This approach aids in a broader and more efficient script deployment.

--- src/wormy/advanced/deploy-jack.js
+++ src/wormy/advanced/deploy-jack.js
@@ -1,15 +1,24 @@
 /** @param {NS} ns **/
 export async function main(ns) {
     const scriptToRun = 'wormy/advanced/jackx.js';
-    while (true) {
-        let servers = ns.scan();
-        for (let i = 0; i < servers.length; i++) {
+    let servers = ns.scan('home');
+    await runScriptOnServers(ns, servers, scriptToRun);
+}
+
+async function runScriptOnServers(ns, servers, scriptToRun) {
+    for(let i = 0; i < servers.length; i++) {
+        if (!ns.isRunning(scriptToRun, 'home', servers[i])) {
             if (ns.getServerRequiredHackingLevel(servers[i]) <= ns.getHackingLevel() && ns.hasRootAccess(servers[i])) {
                 ns.exec(scriptToRun, 'home', 1, servers[i])
-                ns.tprint(`SUCCESS: Deployed JACKX on ${servers[i]}`)
+                ns.tprint(`SUCCESS: Deployed JACKX on ${servers[i]}`);
                 await ns.sleep(50)
+
+                // If a server is not running the script, then we scan for its connected servers and run the script on them as well
+                let connectedServers = ns.scan(servers[i]);
+                await runScriptOnServers(ns, connectedServers, scriptToRun);
             }
+        } else {
+            continue;
         }
-        return;
     }
 }
