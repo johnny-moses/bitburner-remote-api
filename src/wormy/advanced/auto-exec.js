93f1867db98ab976f0c6aad37eb7354ffed4f75f
Johnny <johnny.moses3@gmail.com>
2023-12-30 15:23:17 -0600

    Add an initial print statement in auto-exec.js
    An initial print statement 'AUTOX STARTED' has been added to auto-exec.js in wormy/advanced folder, to confirm the execution of the script. This alerts the user that the automated script execution process has begun.

--- src/wormy/advanced/auto-exec.js
+++ src/wormy/advanced/auto-exec.js
@@ -4,6 +4,7 @@ export async function main(ns) {
     const d_jack = 'wormy/advanced/deploy-jack.js'
 
     ns.exec(wormx, 'home', 1)
+    ns.tprint('AUTOX STARTED')
     await ns.sleep(1000)
     ns.exec(d_jack, 'home', 1)
 }
