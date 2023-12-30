5bc2efc84123051448c18a219d6c5edde1d7d014
Johnny <johnny.moses3@gmail.com>
2023-12-30 12:47:30 -0600

    Refactor server processing in wormx.js
    The wormx.js script has been refactored to improve server handling. A queue has been implemented to handle servers sequentially, thus improving efficiency and clarity in the processing flow. Additionally, execution on home server has been skipped to avoid unnecessary operations.

--- src/wormy/advanced/wormx.js
+++ src/wormy/advanced/wormx.js
@@ -3,45 +3,68 @@ export async function main(ns) {
     const supportingScripts = ['wormy/advanced/scripts/hack.js', 'wormy/advanced/scripts/grow.js',
         'wormy/advanced/scripts/weaken.js', 'wormy/advanced/jackx.js'];
 
+    // Outermost loop to keep the script running indefinitely
     while (true) {
-        let servers = ns.scan();
-        for (let i = 0; i < servers.length; i++) {
-            let portsRequired = ns.getServerNumPortsRequired(servers[i])
-            let portsOpened = 0;
-            if (ns.hasRootAccess(servers[i])) {
-                continue;
-            }
-            if (ns.fileExists('BruteSSH.exe', 'home')) {
-                ns.brutessh(servers[i]);
-                portsOpened++;
-            }
-            if (ns.fileExists('FTPCrack.exe', 'home')) {
-                ns.ftpcrack(servers[i]);
-                portsOpened++;
-            }
-            if (ns.fileExists('relaySMTP.exe', 'home')) {
-                ns.relaysmtp(servers[i]);
-                portsOpened++;
-            }
-            if (ns.fileExists('HTTPWorm.exe', 'home')) {
-                ns.httpworm(servers[i]);
-                portsOpened++;
-            }
-            if (ns.fileExists('SQLInject.exe', 'home')) {
-                ns.sqlinject(servers[i]);
-                portsOpened++;
-            }
+        // create a queue, and add the initially scanned servers from 'home'
+        let serversToProcess = ns.scan('home');
 
-            if (portsRequired <= portsOpened) {
-                ns.nuke(servers[i]);
-                for (let script of supportingScripts) {
-                    ns.scp(script, servers[i])
-                    ns.tprint(`Copied ${script} to ${servers[i]}`, 'pink')
-                    await ns.sleep(20)
+        while (serversToProcess.length > 0) {
+            let currentServer = serversToProcess.shift();
+            if (currentServer !== 'home') {
+                await processServer(ns, currentServer, supportingScripts);
+                ns.print(`WARN: Scanning for new servers from ${currentServer}`);
+                let foundServers = ns.scan(currentServer);
+                for (let server of foundServers) {
+                    if (!serversToProcess.includes(server)) {
+                        serversToProcess.push(server);
+                    }
                 }
-                ns.tprint(`SUCCESS: Root Access gained on ${servers[i]}`)
+                await ns.sleep(100);
             }
         }
-        await ns.sleep(500)
+
+        // Sleep before starting another round of scanning and processing
+        ns.tprint(`INFO: Completed a round of server processing. Starting again in 1 second.`);
+        await ns.sleep(1000);
     }
+}
+
+async function processServer(ns, server, supportingScripts) {
+    let portsRequired = ns.getServerNumPortsRequired(server);
+    let portsOpened = 0;
+    if (ns.hasRootAccess(server)) {
+        // ns.tprint(`Already have root access on ${server}`);
+        return;
+    }
+    if (ns.fileExists('BruteSSH.exe', 'home')) {
+        ns.brutessh(server);
+        portsOpened++;
+    }
+    if (ns.fileExists('FTPCrack.exe', 'home')) {
+        ns.ftpcrack(server);
+        portsOpened++;
+    }
+    if (ns.fileExists('relaySMTP.exe', 'home')) {
+        ns.relaysmtp(server);
+        portsOpened++;
+    }
+    if (ns.fileExists('HTTPWorm.exe', 'home')) {
+        ns.httpworm(server);
+        portsOpened++;
+    }
+    if (ns.fileExists('SQLInject.exe', 'home')) {
+        ns.sqlinject(server);
+        portsOpened++;
+    }
+
+    if (portsRequired <= portsOpened) {
+        ns.nuke(server);
+        for (let script of supportingScripts) {
+            ns.scp(script, server);
+            ns.print(`Copied ${script} to ${server}`);
+            await ns.sleep(20);
+        }
+        ns.tprint(`SUCCESS: Root Access gained on ${server}`);
+    }
+    await ns.sleep(500); // little sleep after processing each server
 }
