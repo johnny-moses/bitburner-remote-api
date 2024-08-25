200273728e576133d89e1f3a46b931cc8cf4f743
Johnny <johnny.moses3@gmail.com>
2024-08-25 18:06:09 -0500

    Remove auto-exec.js and obsolete save file.
    Deleted the auto-exec.js script from the advanced wormy directory as it is no longer needed. Also removed an outdated Bitburner save file to clean up unused resources.

--- src/util/thanos.js
+++
@@ -1,9 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-    const files = ns.ls('home'); // Get the list of all files on 'home' server
-
-    for (const file of files) {
-        ns.rm(file, 'home'); // Remove each file
-        ns.tprint(`Deleted file: ${file}`);
-    }
-}
